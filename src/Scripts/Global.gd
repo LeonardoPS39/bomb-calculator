dfa966d2700134c04e5d4fceb16b3efc031f4c62
LeonardoPS39 <leonardo.silva3903@gmail.com>
2024-05-26 18:19:02 -0300

    added checkpoints logics

--- src/Scripts/Global.gd
+++ src/Scripts/Global.gd
@@ -1,3 +1,5 @@
 extends Node
 
 var speedrun_time = 200
+var checkpoint_position = Vector2(125, -17)
+var map_spawn = Vector2(86, 381)
---
+++ src/Scripts/checkpoint.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+var checkpoint_pos = Global.checkpoint_position
+
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		checkpoint_pos = Vector2(position.x, position.y)
+		Global.checkpoint_position = checkpoint_pos
